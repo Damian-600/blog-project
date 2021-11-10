25137f7c8c78dda256e2aebf6213fef4cb2e4d89
damian-600 <damian.kozlowski@exactive.co.uk>
2021-11-10 13:39:29 +0000

    new blog post

--- controllers/sbcActionsController.js
+++ controllers/sbcActionsController.js
@@ -1,3 +1,101 @@
+const fs = require('fs').promises;
+const fetch = require('node-fetch');
+const FormData = require('form-data');
+
+exports.newTenant = async (req, res, next) => {
+	// Verify madatory data is not missing from the request body
+	if (
+		!req.body.tenantName ||
+		!req.body.cacLimit ||
+		!req.body.trunkFqdn ||
+		!req.body.sbcIp ||
+		!req.headers.authorization
+	) {
+		return res.status(400).send({
+			status: 'failure',
+			message: 'Request body missing madatory data.',
+		});
+	}
+
+	// Read CLI Template from file
+	const cliTemplate = await fs.readFile(
+		'./resources/addTenantTemplate.txt',
+		'utf8',
+		(err, data) => {
+			if (err) {
+				return res.status(500).send({
+					status: 'failure',
+					message: 'Failed to read cli template file from local disk.',
+				});
+			}
+			return data;
+		}
+	);
+
+	// Replace placeholder values with data from request body
+
+	const cliScript = cliTemplate.replace(
+		/(\[TENANT_NAME\])|(\[TRUNK_FQDN\])|(\[CAC_LIMIT\])/gi,
+		function (str, p1, p2, p3) {
+			if (p1) return req.body.tenantName;
+			if (p2) return req.body.trunkFqdn;
+			if (p3) return req.body.cacLimit;
+		}
+	);
+
+	// Prepare body for the request towards SBC
+	const formBody = new FormData();
+	formBody.append('file', cliScript, {
+		filename: 'cli.txt',
+		contentType: 'text/plain',
+	});
+
+	// Prepare headers for the request. Note passthough authentication from postman to SBC.
+	const reqHeaders = {
+		Authorization: req.headers.authorization,
+		Connection: 'keep-alive',
+	};
+
+	// Set request method and add headers
+	const reqOptions = {
+		method: 'PUT',
+		headers: reqHeaders,
+		body: formBody,
+		redirect: 'follow',
+		timeout: 7000,
+	};
+
+	// Upload CLI script to SBC
+	try {
+		const uploadRes = await fetch(
+			`http://${req.body.sbcIp}/api/v1/files/cliScript/incremental`,
+			reqOptions
+		);
+
+		// Client 3xx-5xx errors
+		if (uploadRes.status !== 200) {
+			return res.status(400).send({
+				status: 'failure',
+				message: `Failed to upload CLI to SBC. The following error was received from downstream device: ${uploadRes.statusText}`,
+			});
+		}
+
+		// Handling successful uploads
+
+		const responseData = await uploadRes.json();
+		return res.status(200).send({
+			status: 'success',
+			data: responseData.description,
+		});
+	} catch (err) {
+		// All other errors including network
+		return res.status(500).send({
+			status: 'failure',
+			message: `${err}`,
+		});
+	}
+};
+
 exports.test = async (req, res, next) => {
 	res.status(200).send({
 		status: 'success',
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -4,5 +4,6 @@ const sbcActionsController = require('../controllers/sbcActionsController');
 const router = express.Router();
 
 router.route('/test').get(sbcActionsController.test);
+router.route('/newTenant').put(sbcActionsController.newTenant);
 
 module.exports = router;
